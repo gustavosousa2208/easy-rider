e6ab4b48f11acde752549bfa092d74eb88d18b8f
gustavosousa2208 <gustavosousa@alu.ufc.br>
2022-04-16 18:03:24 -0300

    stage 3/6 added function for counting line stops

--- easyrider.py
+++ easyrider.py
@@ -1,5 +1,6 @@
 import json
 import re
+from itertools import groupby
 
 BUS_DATA = 'input'  # if "input" then, receive from stdin, else, from file declared here
 DUMP_FILE = "bus.json"  # given a path, will dump the data to a file
@@ -34,16 +35,16 @@ class Fleet:
 
     def show(self):
         for x in self.buses:
-            print(x.bus_id, x.stop_id, x.stop_name, x.next_stop, x.stop_type, x.a_time)
+            print(x["bus_id"], x["stop_id"], x["stop_name"], x["stop_type"], x["a_time"])
 
     def check_data(self):
+        """checking data with regex for correct typing"""
         errors = [0] * 3
         for x in self.buses:
             if x["stop_name"] == "" or type(x["stop_name"]) != str:
                 errors[0] += 1
             elif stop_name_match.match(x["stop_name"]) is None:
                 errors[0] += 1
-                print("name:", x["stop_name"], file=open("error.txt", "a"))
 
             if len(x["stop_type"]) > 0:
                 if stop_type_match.search(x["stop_type"]) is None:
@@ -51,19 +52,47 @@ class Fleet:
 
             if x["a_time"] == "" or type(x["a_time"]) != str:
                 errors[2] += 1
-                print("time:", x["a_time"], file=open("error.txt", "a"))
             else:
                 if stop_time_match.match(x["a_time"]) is None:
                     errors[2] += 1
-                    print("time:", x["a_time"], file=open("error.txt", "a"))
 
         print("""Type and required field validation: {} errors
 stop_name: {}
 stop_type: {}
 a_time: {}""".format(sum(errors), *errors))
 
+    def line_stops(self):
+        """function for listing line_stops"""
+        group = []
+
+        # inner to outer loop: sort by id, then separate by id
+        for _, bus in groupby(sorted(self.buses, key=lambda i: i["bus_id"]), lambda k: k["bus_id"]):
+            group.append(list(bus))
+
+        # create list for stops with different memory addresses
+        lines = [[] for x in range(len(group))]
+
+        # store ids for printing
+        ids = [group[i][0]["bus_id"] for i in range(len(group))]
+
+        # store all of the stop values in list
+        for bus in group:
+            for x in bus:
+                lines[group.index(bus)].append(x["stop_id"])
+
+        # only unique values inside lines
+        for x in range(len(lines)):
+            lines[x] = set(lines[x])
+
+        print("Line names and number of stops:")
+        for x in range(len(lines)):
+            print("Line {}: {} stops".format(ids[x], len(lines[x])))
+
+    def bus_schedule_check(self):
+        """function for checking bus start, end line and stops"""
+
 
 if __name__ == '__main__':
     fleet = Fleet()
     fleet.import_data(BUS_DATA)
-    fleet.check_data()
+    fleet.line_stops()
